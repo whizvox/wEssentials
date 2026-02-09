97cfa26e15339c70d484850850abb5dde7cd050a
whizvox <corneliuseanes@gmail.com>
2026-02-09 02:21:23 -0800

    Fix player nicknames not showing up when logging in

--- src/main/java/me/whizvox/wessentials/module/nick/NicknameModule.java
+++ src/main/java/me/whizvox/wessentials/module/nick/NicknameModule.java
@@ -10,6 +10,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.permissions.Permission;
@@ -55,7 +56,7 @@ public class NicknameModule extends SerializableModule implements Listener {
         config.set("nicknames", nicknamesList);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Component nick = getNickname(event.getPlayer());
         if (nick != null) {
